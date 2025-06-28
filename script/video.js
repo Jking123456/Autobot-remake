0c255175217a5b96cd051f09d01a542bb5421f2a
Jking123456 <prinzvans@gmail.com>
2025-06-28 18:27:35 +0800

    Update video.js

--- script/video.js
+++ script/video.js
@@ -1,1 +1,59 @@
+const path = require("path");
+const axios = require("axios");
+const fs = require("fs");
 
+module.exports.config = {
+  name: "video",
+  version: "9",
+  credits: "Cliff", //api by jonell & churo
+  description: "Search video from YouTube",
+  commandCategory: "media",
+  hasPermssion: 0,
+  cooldowns: 9,
+  usages: "[video [search]",
+  role: 0,
+  hasPrefix: false,
+};
+
+module.exports.run = async function ({ api, args, event }) {
+  try {
+    const searchQuery = args.join(" ");
+    if (!searchQuery) {
+      api.sendMessage("Usage: video <search text>", event.threadID);
+      return;
+    }
+
+    const ugh = api.sendMessage(`⏱️ | Searching, for '${searchQuery}' please wait...`, event.threadID);
+
+    api.setMessageReaction("🕥", event.messageID, (err) => {}, true);
+
+    const response = await axios.get(`https://haji-mix.up.railway.app/api/youtube?search=${encodeURIComponent(searchQuery)}`);
+
+    const data = response.data;
+    const videoUrl = data.url;
+    const title = data.title;
+    const thumbnail = data.thumbnail;
+
+    const videoPath = path.join(__dirname, "cache", "video.mp4");
+
+    const videoResponse = await axios.get(videoUrl, { responseType: "arraybuffer" });
+
+    fs.writeFileSync(videoPath, Buffer.from(videoResponse.data));
+
+    api.setMessageReaction("✅", event.messageID, (err) => {}, true);
+
+    await api.sendMessage(
+      {
+        body: `Here's your video, enjoy!🥰\n\n𝗧𝗶𝘁𝘁𝗹𝗲: ${title}`,
+        attachment: fs.createReadStream(videoPath),
+      },
+      event.threadID,
+      event.messageID
+    );
+    fs.unlinkSync(videoPath);
+    api.unsendMessage(ugh.messageID);
+  } catch (error) {
+    api.sendMessage(`error: ${error.message}`, event.threadID, event.messageID);
+    console.log(error);
+  }
+};
