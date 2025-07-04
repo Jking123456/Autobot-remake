4f17ae50181aafafed1c4bd62c32d8b7ff5b77db
Jking123456 <prinzvans@gmail.com>
2025-07-04 09:40:16 +0800

    Update autogreet.js

--- script/autogreet.js
+++ script/autogreet.js
@@ -1,120 +1,39 @@
-var cron = require("node-cron");
+const cron = require("node-cron");
 
-// Function to send greetings
-function sendGreeting(message) {
-  // Replace this with your code to send the greeting message
-  console.log(message);
-}
+module.exports.config = {
+  name: "greetingscheduler",
+  version: "1.0.0",
+  hasPermission: 2,
+  credits: "YourName",
+  description: "Sends scheduled greetings to all groups",
+  commandCategory: "system",
+  usages: "",
+  cooldowns: 5,
+};
 
-cron.schedule('0 0 6 * * *', () => {
-  sendGreeting("Good morning everyone! It's time to wake up.");
-}, {
-  scheduled: true,
-  timezone: "Asia/Manila"
-});
+module.exports.onLoad = function({ api }) {
+  const sendToGroups = (msg) => {
+    api.getThreadList(30, null, ["INBOX"], (err, list) => {
+      if (err) return console.log("ERR: " + err);
+      list.forEach(thread => {
+        if (thread.isGroup) {
+          api.sendMessage(msg, thread.threadID);
+        }
+      });
+    });
+  };
 
-cron.schedule('0 0 9 * * *', () => {
-  sendGreeting("Good morning! Have a great day ahead.");
-}, {
-  scheduled: true,
-  timezone: "Asia/Manila"
-});
+  cron.schedule('0 0 6 * * *', () => sendToGroups("Good morning everyone! It's time to wake up."), { timezone: "Asia/Manila" });
+  cron.schedule('0 0 9 * * *', () => sendToGroups("Good morning! Have a great day ahead."), { timezone: "Asia/Manila" });
+  cron.schedule('0 0 12 * * *', () => sendToGroups("Good afternoon! It's lunchtime."), { timezone: "Asia/Manila" });
+  cron.schedule('0 0 13 * * *', () => sendToGroups("Good afternoon everyone!"), { timezone: "Asia/Manila" });
+  cron.schedule('0 0 15 * * *', () => sendToGroups("Hello there! It's 3:00 PM."), { timezone: "Asia/Manila" });
+  cron.schedule('0 0 17 * * *', () => sendToGroups("Good afternoon! It's 5:00 PM."), { timezone: "Asia/Manila" });
+  cron.schedule('0 0 18 * * *', () => sendToGroups("Good evening! It's 6:00 PM."), { timezone: "Asia/Manila" });
+  cron.schedule('0 0 19 * * *', () => sendToGroups("Good evening! It's dinner time."), { timezone: "Asia/Manila" });
+  cron.schedule('0 0 21 * * *', () => sendToGroups("Good evening! It's 9:00 PM."), { timezone: "Asia/Manila" });
+  cron.schedule('0 0 22 * * *', () => sendToGroups("Goodnight everyone! Sweet dreams."), { timezone: "Asia/Manila" });
+  cron.schedule('0 0 0 * * *', () => sendToGroups("Goodnight everyone! It's midnight."), { timezone: "Asia/Manila" });
 
-cron.schedule('0 0 12 * * *', () => {
-  api.getThreadList(30, null, ["INBOX"], (err, list) => {
-    if (err) return console.log("ERR: " + err);
-    list.forEach(now => (now.isGroup == true && now.threadID != list.threadID) ?
-      api.sendMessage("Good afternoon! It's lunchtime.", now.threadID) : '');
-  });
-}, {
-  scheduled: true,
-  timezone: "Asia/Manila"
-});
-
-cron.schedule('0 0 13 * * *', () => {
-  api.getThreadList(30, null, ["INBOX"], (err, list) => {
-    if (err) return console.log("ERR: " + err);
-    list.forEach(now => (now.isGroup == true && now.threadID != list.threadID) ?
-      api.sendMessage("Good afternoon everyone!", now.threadID) : '');
-  });
-}, {
-  scheduled: true,
-  timezone: "Asia/Manila"
-});
-
-cron.schedule('0 0 15 * * *', () => {
-  api.getThreadList(30, null, ["INBOX"], (err, list) => {
-    if (err) return console.log("ERR: " + err);
-    list.forEach(now => (now.isGroup == true && now.threadID != list.threadID) ?
-      api.sendMessage("Hello there! It's 3:00 PM.", now.threadID) : '');
-  });
-}, {
-  scheduled: true,
-  timezone: "Asia/Manila"
-});
-
-cron.schedule('0 0 17 * * *', () => {
-  api.getThreadList(30, null, ["INBOX"], (err, list) => {
-    if (err) return console.log("ERR: " + err);
-    list.forEach(now => (now.isGroup == true && now.threadID != list.threadID) ?
-      api.sendMessage("Good afternoon! It's 5:00 PM.", now.threadID) : '');
-  });
-}, {
-  scheduled: true,
-  timezone: "Asia/Manila"
-});
-
-cron.schedule('0 0 18 * * *', () => {
-  api.getThreadList(30, null, ["INBOX"], (err, list) => {
-    if (err) return console.log("ERR: " + err);
-    list.forEach(now => (now.isGroup == true && now.threadID != list.threadID) ?
-      api.sendMessage("Good evening! It's 6:00 PM.", now.threadID) : '');
-  });
-}, {
-  scheduled: true,
-  timezone: "Asia/Manila"
-});
-
-cron.schedule('0 0 19 * * *', () => {
-  api.getThreadList(30, null, ["INBOX"], (err, list) => {
-    if (err) return console.log("ERR: " + err);
-    list.forEach(now => (now.isGroup == true && now.threadID != list.threadID) ?
-      api.sendMessage("Good evening! It's dinner time.", now.threadID) : '');
-  });
-}, {
-  scheduled: true,
-  timezone: "Asia/Manila"
-});
-
-cron.schedule('0 0 21 * * *', () => {
-  api.getThreadList(30, null, ["INBOX"], (err, list) => {
-    if (err) return console.log("ERR: " + err);
-    list.forEach(now => (now.isGroup == true && now.threadID != list.threadID) ?
-      api.sendMessage("Good evening! It's 9:00 PM.", now.threadID) : '');
-  });
-}, {
-  scheduled: true,
-  timezone: "Asia/Manila"
-});
-
-cron.schedule('0 0 22 * * *', () => {
-  api.getThreadList(30, null, ["INBOX"], (err, list) => {
-    if (err) return console.log("ERR: " + err);
-    list.forEach(now => (now.isGroup == true && now.threadID != list.threadID) ?
-      api.sendMessage("Goodnight everyone! Sweet dreams.", now.threadID) : '');
-  });
-}, {
-  scheduled: true,
-  timezone: "Asia/Manila"
-});
-
-cron.schedule('0 0 0 * * *', () => {
-  api.getThreadList(30, null, ["INBOX"], (err, list) => {
-    if (err) return console.log("ERR: " + err);
-    list.forEach(now => (now.isGroup == true && now.threadID != list.threadID) ?
-      api.sendMessage("Goodnight everyone! It's midnight.", now.threadID) : '');
-  });
-}, {
-  scheduled: true,
-  timezone: "Asia/Manila"
-});
+  console.log("Greeting scheduler loaded.");
+};
