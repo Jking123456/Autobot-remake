48582e7cc61f1a84dd68f90bd1936e92c0739b88
Jking123456 <prinzvans@gmail.com>
2025-07-10 13:50:23 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -35,7 +35,7 @@ function updateTime() {
   const now = new Date();
   const options = {
     timeZone: 'Asia/Manila',
-    hour12: false,
+    hour12: true,
     hour: 'numeric',
     minute: 'numeric',
     second: 'numeric'
