4a99b0b44faf36677ebcd88e84b7564edf89110e
Jking123456 <prinzvans@gmail.com>
2025-07-24 17:53:03 +0800

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
+
+# Install Google Chrome
 apt-get update
 apt-get install -y wget curl gnupg2
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /usr/share/keyrings/google-linux-keyring.gpg
+
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-linux-keyring.gpg] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+
 apt-get update
 apt-get install -y google-chrome-stable
+
+# Install dependencies
 npm install
