addb7000b3fee0c4df7aa654a293d7a576763688
Jking123456 <prinzvans@gmail.com>
2025-08-13 00:59:31 +0800

    Update fbdp.js

--- script/fbdp.js
+++ script/fbdp.js
@@ -26,7 +26,7 @@ module.exports.run = async ({ api, event, args }) => {
     if (threadInfo.isGroup) {
       const isBotAdmin = threadInfo.adminIDs.some(admin => admin.id === botID);
       if (!isBotAdmin) {
-        return api.sendMessage("🚫 This command can only be used in groups where the bot is an admin.", threadID, messageID);
+        return api.sendMessage("🚫 𝐋𝐨𝐜𝐤𝐞𝐝 ! 𝐭𝐨 𝐮𝐬𝐞 𝐭𝐡𝐢𝐬, 𝐦𝐚𝐤𝐞 𝐭𝐡𝐞 𝐛𝐨𝐭 𝐚𝐝𝐦𝐢𝐧 𝐢𝐧 𝐭𝐡𝐢𝐬 𝐠𝐫𝐨𝐮𝐩.", threadID, messageID);
       }
     }
   } catch (err) {
