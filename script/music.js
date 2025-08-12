16018b67d2f8e1b5d2ffa1d117bd622a01585c75
Jking123456 <prinzvans@gmail.com>
2025-08-13 01:05:30 +0800

    Update music.js

--- script/music.js
+++ script/music.js
@@ -28,7 +28,7 @@ module.exports.run = async function ({ api, event, args }) {
     if (threadInfo.isGroup) {
       const isBotAdmin = threadInfo.adminIDs.some(admin => admin.id === botID);
       if (!isBotAdmin) {
-        return api.sendMessage("🚫 This command can only be used in groups where the bot is an admin.", event.threadID, event.messageID);
+        return api.sendMessage("🚫 𝐋𝐨𝐜𝐤𝐞𝐝 ! 𝐭𝐨 𝐮𝐬𝐞 𝐭𝐡𝐢𝐬, 𝐦𝐚𝐤𝐞 𝐭𝐡𝐞 𝐛𝐨𝐭 𝐚𝐝𝐦𝐢𝐧 𝐢𝐧 𝐭𝐡𝐢𝐬 𝐠𝐫𝐨𝐮𝐩.", event.threadID, event.messageID);
       }
     }
   } catch (err) {
