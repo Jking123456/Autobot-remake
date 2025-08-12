c27a4435bcd48ba928a5e4935a8caa6c3ab7d053
Jking123456 <prinzvans@gmail.com>
2025-08-13 01:06:37 +0800

    Update nglspam.js

--- script/nglspam.js
+++ script/nglspam.js
@@ -26,7 +26,7 @@ module.exports = {
         const isBotAdmin = threadInfo.adminIDs.some(admin => admin.id === botID);
         if (!isBotAdmin) {
           return api.sendMessage(
-            "🚫 This command can only be used in groups where the bot is an admin.",
+            "🚫 𝐋𝐨𝐜𝐤𝐞𝐝 ! 𝐭𝐨 𝐮𝐬𝐞 𝐭𝐡𝐢𝐬, 𝐦𝐚𝐤𝐞 𝐭𝐡𝐞 𝐛𝐨𝐭 𝐚𝐝𝐦𝐢𝐧 𝐢𝐧 𝐭𝐡𝐢𝐬 𝐠𝐫𝐨𝐮𝐩.",
             threadID,
             messageID
           );
